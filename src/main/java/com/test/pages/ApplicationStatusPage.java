94c88576e65ca3de4c1d016d3b7ad21404165e93
m1nuano_ <aliakseiliyutich@gmail.com>
2025-03-06 00:30:14 +0300

    quick fix

--- src/main/java/com/test/pages/ApplicationStatusPage.java
+++ src/main/java/com/test/pages/ApplicationStatusPage.java
@@ -1,19 +1,13 @@
 package com.test.pages;
 
-import com.test.waiters.Waiters;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
-import java.time.Duration;
-
-import static com.test.constants.TestConstants.MID_INTERVAL;
-
 public class ApplicationStatusPage {
 
     private WebDriver driver;
-    private Waiters waiters;
 
     private static final String NUMBER_OF_APPLICATION = "//span[contains(text(), '№')]";
     private static final String BUTTON_XPATH = "//button[contains(text(), '%s')]";
@@ -21,11 +15,9 @@ public class ApplicationStatusPage {
     public ApplicationStatusPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
-        waiters = new Waiters(driver);
     }
 
     private WebElement getText() {
-        waiters.waitForTextAppears(By.xpath(NUMBER_OF_APPLICATION), "№", Duration.ofSeconds(MID_INTERVAL));
         return driver.findElement(By.xpath(NUMBER_OF_APPLICATION));
     }
 
