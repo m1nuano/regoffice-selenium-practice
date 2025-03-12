346eeae352b9c37329d83f9d0dc293a0f26bb77b
m1nuano_ <aliakseiliyutich@gmail.com>
2025-03-12 21:52:11 +0300

    Finalize DB tests, add messages when a test crashes
    - created DbBaseTest class in which DbApplicantSteps and DbStaffSteps are initialized

--- src/test/java/DatabaseApplicantTests.java
+++ src/test/java/DatabaseApplicantTests.java
@@ -1,36 +1,28 @@
 import com.test.entity.Applicant;
-import com.test.steps.DatabaseApplicantSteps;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Step;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-public class DatabaseApplicantTests {
-    private static DatabaseApplicantSteps dbSteps;
-
-    @BeforeTest
-    static void setup() {
-        dbSteps = new DatabaseApplicantSteps();
-    }
+public class DatabaseApplicantTests extends DbBaseTest{
 
     @Epic("Database tests")
     @Feature("Applicant creation and saving")
     @Test
     @Step("Creating applicant and checking this applicant is created correctly")
     void testCreateApplicant() {
-        Applicant applicant = dbSteps.createAndSaveApplicant();
-        Applicant fromDb = dbSteps.getApplicantFromDb(applicant.getApplicantId());
+        Applicant applicant = dbApplicantSteps.createAndSaveApplicant();
+        Applicant fromDb = dbApplicantSteps.getApplicantFromDb(applicant.getApplicantId());
 
         SoftAssert soft = new SoftAssert();
-        soft.assertNotNull(fromDb);
-        soft.assertEquals(applicant.getSurname(), fromDb.getSurname());
-        soft.assertEquals(applicant.getName(), fromDb.getName());
-        soft.assertEquals(applicant.getMiddleName(), fromDb.getMiddleName());
-        soft.assertEquals(applicant.getPassportNumber(), fromDb.getPassportNumber());
-        soft.assertEquals(applicant.getPhoneNumber(), fromDb.getPhoneNumber());
-        soft.assertEquals(applicant.getRegistrationAddress(), fromDb.getRegistrationAddress());
+        soft.assertNotNull(fromDb, "Applicant was not found in the database");
+        soft.assertEquals(applicant.getSurname(), fromDb.getSurname(), "The surname does not match");
+        soft.assertEquals(applicant.getName(), fromDb.getName(), "The name does not match");
+        soft.assertEquals(applicant.getMiddleName(), fromDb.getMiddleName(), "The middle name does not match");
+        soft.assertEquals(applicant.getPassportNumber(), fromDb.getPassportNumber(), "Passport number does not match");
+        soft.assertEquals(applicant.getPhoneNumber(), fromDb.getPhoneNumber(), "Phone number does not match");
+        soft.assertEquals(applicant.getRegistrationAddress(), fromDb.getRegistrationAddress(), "The registration address does not match");
         soft.assertAll();
     }
 }
--- src/test/java/DatabaseStaffTests.java
+++ src/test/java/DatabaseStaffTests.java
@@ -1,36 +1,28 @@
 import com.test.entity.Staff;
-import com.test.steps.DatabaseStaffSteps;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Step;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-public class DatabaseStaffTests {
-    private static DatabaseStaffSteps dbSteps;
-
-    @BeforeTest
-    static void setup() {
-        dbSteps = new DatabaseStaffSteps();
-    }
+public class DatabaseStaffTests extends DbBaseTest {
 
     @Epic("Database tests")
     @Feature("Staff creation and saving")
     @Test
     @Step("Creating staff and checking this staff is created correctly")
     void testCreateStaff() {
-        Staff staff = dbSteps.createAndSaveStaff();
-        Staff fromDb = dbSteps.getStaffFromDb(staff.getStaffId());
+        Staff staff = dbStaffSteps.createAndSaveStaff();
+        Staff fromDb = dbStaffSteps.getStaffFromDb(staff.getStaffId());
 
         SoftAssert soft = new SoftAssert();
-        soft.assertNotNull(fromDb);
-        soft.assertEquals(staff.getSurname(), fromDb.getSurname());
-        soft.assertEquals(staff.getName(), fromDb.getName());
-        soft.assertEquals(staff.getMiddleName(), fromDb.getMiddleName());
-        soft.assertEquals(staff.getDateOfBirth(), fromDb.getDateOfBirth());
-        soft.assertEquals(staff.getPassportNumber(), fromDb.getPassportNumber());
-        soft.assertEquals(staff.getPhoneNumber(), fromDb.getPhoneNumber());
+        soft.assertNotNull(fromDb, "Staff was not found in the database");
+        soft.assertEquals(staff.getSurname(), fromDb.getSurname(), "The surname does not match");
+        soft.assertEquals(staff.getName(), fromDb.getName(), "The name does not match");
+        soft.assertEquals(staff.getMiddleName(), fromDb.getMiddleName(), "The middle name does not match");
+        soft.assertEquals(staff.getDateOfBirth(), fromDb.getDateOfBirth(), "The date of birth does not match");
+        soft.assertEquals(staff.getPassportNumber(), fromDb.getPassportNumber(), "The passport number does not match");
+        soft.assertEquals(staff.getPhoneNumber(), fromDb.getPhoneNumber(), "The phone number does not match");
         soft.assertAll();
     }
 }
