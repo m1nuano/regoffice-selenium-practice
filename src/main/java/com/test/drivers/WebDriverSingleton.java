0cf4be1cabe078a8c3af5b3c283d0309e3074b72
m1nuano_ <aliakseiliyutich@gmail.com>
2025-03-14 19:04:57 +0300

    Update driver from edge to chrome

--- src/main/java/com/test/drivers/WebDriverSingleton.java
+++ src/main/java/com/test/drivers/WebDriverSingleton.java
@@ -3,8 +3,8 @@ package com.test.drivers;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 @Log4j2
 public class WebDriverSingleton {
@@ -13,13 +13,13 @@ public class WebDriverSingleton {
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            log.info("WebDriver initialisation (EdgeDriver)");
-            WebDriverManager.edgedriver().setup();
-            EdgeOptions options = new EdgeOptions();
+            log.info("WebDriver initialisation (ChromeDriver)");
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
             options.setCapability("useChromium", true);
             options.addArguments("--disable-gpu");
             options.addArguments("--no-sandbox");
-            driver = new EdgeDriver(options);
+            driver = new ChromeDriver();
             log.info("WebDriver has been successfully created");
         } else {
             log.debug("Return the existing copy of Webdriver");
